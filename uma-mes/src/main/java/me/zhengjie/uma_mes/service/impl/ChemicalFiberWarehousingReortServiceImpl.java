5cbc9d94692c65333b71912baee33a761bacc719
tianshangdexiaoniaoqun <dengzhaoxiong@126.com>
2020-08-29 12:49:45 +0800

    修改盘库单的bug

--- uma-mes/src/main/java/me/zhengjie/uma_mes/service/impl/ChemicalFiberWarehousingReortServiceImpl.java
+++ uma-mes/src/main/java/me/zhengjie/uma_mes/service/impl/ChemicalFiberWarehousingReortServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.*;
 
@@ -68,12 +69,10 @@ public class ChemicalFiberWarehousingReortServiceImpl implements ChemicalFiberWa
                 chemicalFiberStockWarehousingDetailRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,criteria,criteriaBuilder),pageable);
         List<ChemicalFiberStockWarehousingDetail> list1 = page1.getContent();
         List<ChemicalFiberWarehousingReortDTO> warehousingReortList = new ArrayList<>();
-        Integer Elements = 0;
         for (int i = 0; i < list1.size(); i++) {
             ChemicalFiberStockWarehousingDetail dto = list1.get(i);
             ChemicalFiberStockWarehousing warehousing = indexWarehousingId(list, dto.getWarehousingId());
             if (warehousing != null) {
-                ++Elements;
                 ChemicalFiberWarehousingReortDTO WarehousingReort = new ChemicalFiberWarehousingReortDTO();
                 WarehousingReort.setScanNumber(dto.getScanNumber());
                 WarehousingReort.setWarehousingDate(warehousing.getWarehousingDate());
@@ -90,6 +89,61 @@ public class ChemicalFiberWarehousingReortServiceImpl implements ChemicalFiberWa
         }
         return PageUtil.toPage(new PageImpl(warehousingReortList, pageable, page1.getTotalElements()));
     }
+
+    public Map<String,Object> getSummaryData(ChemicalFiberWarehousingReortQueryCriteria criteria){
+        Map<String, Object> map = new HashMap<>();
+        ChemicalFiberStockWarehousingQueryCriteria ca = new ChemicalFiberStockWarehousingQueryCriteria();
+        String createUser = "";
+        String supplierName = "";
+        BigDecimal sum = new BigDecimal(0);
+        BigDecimal sumTon = new BigDecimal(0);
+        BigDecimal sumTotalPrice = new BigDecimal(0);
+        if (criteria.getTempStartTime() != null) {
+            ca.setStartTime(new Timestamp(criteria.getTempStartTime()));
+            criteria.setStartTime(new Timestamp(criteria.getTempStartTime()));
+            ca.setEndTime(new Timestamp(criteria.getTempEndTime()));
+            criteria.setEndTime(new Timestamp(criteria.getTempEndTime()));
+        }
+        if (criteria.getCreateUser() != null) {
+            ca.setCreateUser(criteria.getCreateUser());
+            createUser = criteria.getCreateUser();
+        }
+        if (criteria.getSupplierName() != null) {
+            ca.setSupplierName(criteria.getSupplierName());
+            supplierName = criteria.getSupplierName();
+        }
+        List<Integer> invalidList = new ArrayList<>();
+        invalidList.add(0);
+        if (null != criteria.getQueryWithInvalid() && criteria.getQueryWithInvalid())
+        {
+            invalidList.add(1);
+        }
+        ca.setInvalidList(invalidList);
+        ca.setWarehousingStatus(2);
+        List<ChemicalFiberStockWarehousing> list =
+                chemicalFiberStockWarehousingRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,ca,criteriaBuilder));
+        List<Integer> notid = chemicalFiberStockWarehousingRepository.getNotId(createUser, supplierName);
+        criteria.setWarehousingIdList(notid);
+        List<ChemicalFiberStockWarehousingDetail> list1 = chemicalFiberStockWarehousingDetailRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,criteria,criteriaBuilder));
+        for (int i = 0; i < list1.size(); i++) {
+            ChemicalFiberStockWarehousingDetail dto = list1.get(i);
+            ChemicalFiberStockWarehousing warehousing = indexWarehousingId(list, dto.getWarehousingId());
+            if (warehousing != null) {
+                sumTotalPrice = sumTotalPrice.add(warehousing.getTotalPrice());
+                if (dto.getUnit().equals("吨")) {
+                    sumTon = sumTon.add(dto.getWarehousingNumber());
+                } else {
+                    sum = sum.add(dto.getWarehousingNumber());
+                }
+
+            }
+        }
+        map.put("sumTotalPrice",sumTotalPrice);
+        map.put("sumTon",sumTon);
+        map.put("sum",sum);
+        return map;
+    }
+
 
     public ChemicalFiberStockWarehousing indexWarehousingId(List<ChemicalFiberStockWarehousing> list,Integer warehousingId) {
 
