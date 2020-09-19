263a0fcc890807671fed15cc7b197d53497fa6db
tianshangdexiaoniaoqun <dengzhaoxiong@126.com>
2020-09-19 11:27:38 +0800

    修改入库单bug

--- uma-mes/src/main/java/me/zhengjie/uma_mes/service/impl/ChemicalFiberWarehousingReortServiceImpl.java
+++ uma-mes/src/main/java/me/zhengjie/uma_mes/service/impl/ChemicalFiberWarehousingReortServiceImpl.java
@@ -73,7 +73,7 @@ public class ChemicalFiberWarehousingReortServiceImpl implements ChemicalFiberWa
         for (int i = 0; i < list1.size(); i++) {
             ChemicalFiberStockWarehousingDetail dto = list1.get(i);
             ChemicalFiberStockWarehousing warehousing = indexWarehousingId(list, dto.getWarehousingId());
-            if (warehousing != null) {
+            if (warehousing.getId() != null) {
                 ++Elements;
                 ChemicalFiberWarehousingReortDTO WarehousingReort = new ChemicalFiberWarehousingReortDTO();
                 WarehousingReort.setScanNumber(dto.getScanNumber());
@@ -127,9 +127,10 @@ public class ChemicalFiberWarehousingReortServiceImpl implements ChemicalFiberWa
         List<Integer> notid = chemicalFiberStockWarehousingRepository.getNotId(createUser, supplierName);
         criteria.setWarehousingIdList(notid);
         List<ChemicalFiberStockWarehousingDetail> list1 = chemicalFiberStockWarehousingDetailRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,criteria,criteriaBuilder));
-        for (int i = 0; i < list1.size(); i++) {            ChemicalFiberStockWarehousingDetail dto = list1.get(i);
+        for (int i = 0; i < list1.size(); i++) {
+            ChemicalFiberStockWarehousingDetail dto = list1.get(i);
             ChemicalFiberStockWarehousing warehousing = indexWarehousingId(list, dto.getWarehousingId());
-            if (warehousing != null) {
+            if (warehousing.getId() != null) {
                 sumTotalPrice = sumTotalPrice.add(dto.getTotalPrice());
                 if (dto.getUnit().equals("吨")) {
                     sumTon = sumTon.add(dto.getWarehousingNumber());
@@ -148,12 +149,14 @@ public class ChemicalFiberWarehousingReortServiceImpl implements ChemicalFiberWa
 
 
     public ChemicalFiberStockWarehousing indexWarehousingId(List<ChemicalFiberStockWarehousing> list,Integer warehousingId) {
-
+        ChemicalFiberStockWarehousing is = new ChemicalFiberStockWarehousing();
         for (int j = 0; j < list.size(); j++) {
-            if (list.get(j).getId() == warehousingId) {
-                return list.get(j);
+            Integer a = list.get(j).getId();
+            if (Objects.equals(a, warehousingId)) {
+                is = list.get(j);
+                return is;
             }
         }
-        return null;
+        return is;
     }
 }
